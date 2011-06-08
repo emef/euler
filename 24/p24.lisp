11ebceb946bf7f42bd0df545fd28dca687b8ee31
matt <m@ttforbes.com>
2011-06-07 20:39:25 -0700

    finished some problems and added a util library

--- 24/p24.lisp
+++ 24/p24.lisp
@@ -8,3 +8,43 @@
 
 ;What is the millionth lexicographic permutation of the digits 0, 1,
 ;2, 3, 4, 5, 6, 7, 8 and 9?
+
+(defparameter *target* 1000000)
+(defparameter *digits* '(0 1 2 3 4 5 6 7 8 9))
+
+(let ((f-table (make-hash-table)))
+  (setf (gethash 1 f-table ) 1)
+  (defun factorial (n)
+    (or (gethash n f-table)
+        (setf (gethash n f-table) (* n (factorial (1- n)))))))
+
+(defun first-match (pred lst)
+  (and (consp lst)
+       (let ((x (car lst)))
+         (if (funcall pred x)
+             x
+             (first-match pred (cdr lst))))))
+
+(defun find-perm (target digits)
+  (let ((n-digits (length digits)))
+    (cond ((and (eql target 1) (eql 1 n-digits)) digits)
+          ((or (<= n-digits 1) (<= target 0) nil))
+          (t (let* ((combos (factorial (1- n-digits)))
+                    (possible (loop for i from (1- n-digits) downto 0
+                                 when (< (* i combos) target)
+                                 collect i )))
+               (loop for p in possible do
+                    (let* ((digit (elt digits p))
+                           (result (find-perm (- target (* combos p))
+                                            (remove digit digits))))
+                      (when result
+                        (return (cons digit result))))))))))
+
+;; solution
+(find-perm *target* *digits*)
+               
+                 
+             
+           
+    
+             
--- 26/p26.lisp
+++ 26/p26.lisp
@@ -18,3 +18,38 @@
 ;Find the value of d 1000 for which 1/d contains the longest recurring
 ;cycle in its decimal fraction part.
 
+;; ** huge hint: periodicity of decimal recurrence is equal to
+;; ** periodicity of remainders!!!
+;; 7 \ 10
+;;      30
+;;       20
+;;        60
+;;         40
+;;          50
+;;           10
+;; these are remainders * 10 that you would get while doing long division.
+;; the periodicity of these is equal to periodicity of decimal recurrence
+
+(declaim (OPTIMIZE (DEBUG 3)))
+
+(ql:quickload "euler-utils")
+(use-package :euler-utils)
+
+(defun periodicity (n)
+  (do* ((remainder 10 (* 10 (rem remainder n)))
+        (rem-lst '(10) (push remainder rem-lst)))
+       ((> (count remainder rem-lst) 1)
+        (let* ((first-pos (position remainder rem-lst))
+               (last-pos (position remainder rem-lst :start (1+ first-pos))))
+          (- last-pos first-pos)))))
+
+(defun max-periodicity (max-n)
+  (maximum (mapcar #'(lambda (n) (cons (periodicity n) n))
+                   (range 1 max-n))
+           :> #'(lambda (a b)
+                  (> (car a) (car b)))))
+           
+;; solution
+(max-periodicity 1000)
+           
+    
